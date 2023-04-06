2cc706e5ffddc5a488e326501f9e78ab085c12b0
vishal <vishal.gxm@gmail.com>
2023-04-06 23:45:38 +0530

    used contacts and implemented popup menu

--- src/screens/Chats/Chats.tsx
+++ src/screens/Chats/Chats.tsx
@@ -1,19 +1,62 @@
-import {View, Text, Button} from "react-native";
-import React from "react";
+import {View, Text, Pressable, TouchableOpacity} from "react-native";
+import React, {useState} from "react";
 
+import Entypo from "react-native-vector-icons/Entypo";
 import auth from "@react-native-firebase/auth";
 
 import {dark, light} from "./stylesChats";
 
-const Chats = () => {
+const Chats = ({navigation}: any) => {
+  const [popupVisible, setpopupVisible] = useState(false);
+
   let theme = "dark";
 
   const styles = theme == "dark" ? dark : light;
 
+  const togglePopup = () => {
+    setpopupVisible(!popupVisible);
+  };
+
+  const popupBlur = () => {
+    if (popupVisible) {
+      setpopupVisible(false);
+    }
+  };
+
+  const openNewChat = () => {
+    setpopupVisible(false);
+    navigation.navigate("NewChat");
+  };
+
+  const signout = () => {
+    auth().signOut();
+  };
+
   return (
-    <View style={styles.main}>
-      <Text>Chats</Text>
-    </View>
+    <Pressable style={styles.main} onPress={popupBlur}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerText}>Messages</Text>
+        <Entypo name="dots-three-vertical" size={20} onPress={togglePopup} />
+        <View
+          style={[
+            styles.popupContainer,
+            {display: popupVisible ? "flex" : "none"},
+          ]}>
+          <TouchableOpacity>
+            <Text style={styles.popupText} onPress={openNewChat}>
+              New Message
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.popupText}>New Group</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={signout}>
+            <Text style={styles.popupText}>Sign out</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View></View>
+    </Pressable>
   );
 };
 
--- src/screens/Chats/stylesChats.ts
+++ src/screens/Chats/stylesChats.ts
@@ -2,10 +2,60 @@ import {StyleSheet} from "react-native";
 
 export const dark = StyleSheet.create({
   main: {
-    backgroundColor: "",
+    padding: 5,
+    flex: 1,
+    backgroundColor: "#212121",
+  },
+  headerContainer: {
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    position: "relative",
+  },
+  headerText: {
+    fontSize: 24,
+  },
+  popupContainer: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+    backgroundColor: "#333232",
+    padding: 15,
+    borderRadius: 5,
+    gap: 10,
+  },
+  popupText: {
+    fontSize: 16,
   },
 });
 
 export const light = StyleSheet.create({
-  main: {},
+  main: {
+    padding: 5,
+    flex: 1,
+    backgroundColor: "white",
+  },
+  headerContainer: {
+    paddingHorizontal: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    position: "relative",
+  },
+  headerText: {
+    fontSize: 24,
+  },
+  popupContainer: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+    backgroundColor: "#333232",
+    padding: 15,
+    borderRadius: 5,
+    gap: 10,
+  },
+  popupText: {
+    fontSize: 16,
+  },
 });
