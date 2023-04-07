5f9d883548f6fe708926fe40bf2fcc8440af8a4c
vishal <vishal.gxm@gmail.com>
2023-04-07 22:37:06 +0530

    added option to route new users to contacts screen

--- src/screens/Chats/Chats.tsx
+++ src/screens/Chats/Chats.tsx
@@ -1,15 +1,29 @@
-import {View, Text, Pressable, TouchableOpacity} from "react-native";
+import {
+  View,
+  Text,
+  Pressable,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+} from "react-native";
 import React, {useState} from "react";
 
 import Entypo from "react-native-vector-icons/Entypo";
+import EvilIcons from "react-native-vector-icons/EvilIcons";
 import auth from "@react-native-firebase/auth";
 
 import {dark, light} from "./stylesChats";
+import {useDispatch, useSelector} from "react-redux";
+import {signoutUserAction} from "../../reduxStore/user/userActions";
 
 const Chats = ({navigation}: any) => {
   const [popupVisible, setpopupVisible] = useState(false);
+  const [conversations, setConversations] = useState([]);
+  const [search, setSearch] = useState("");
 
-  let theme = "dark";
+  const {theme} = useSelector((store: any) => store.theme);
+
+  const dispatch = useDispatch();
 
   const styles = theme == "dark" ? dark : light;
 
@@ -29,15 +43,20 @@ const Chats = ({navigation}: any) => {
   };
 
   const signout = () => {
-    // dispatch signout
     auth().signOut();
+    dispatch(signoutUserAction());
   };
 
   return (
     <Pressable style={styles.main} onPress={popupBlur}>
       <View style={styles.headerContainer}>
         <Text style={styles.headerText}>Messages</Text>
-        <Entypo name="dots-three-vertical" size={20} onPress={togglePopup} />
+        <Entypo
+          name="dots-three-vertical"
+          size={20}
+          color="#60bf4b"
+          onPress={togglePopup}
+        />
         <View
           style={[
             styles.popupContainer,
@@ -56,7 +75,29 @@ const Chats = ({navigation}: any) => {
           </TouchableOpacity>
         </View>
       </View>
-      <View></View>
+      <View style={styles.searchContainer}>
+        <EvilIcons name="search" size={30} color="grey" />
+        <TextInput
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search for a user"
+          style={styles.searchInput}
+          onSubmitEditing={Keyboard.dismiss}
+          placeholderTextColor="grey"
+        />
+      </View>
+      <View style={styles.chatContainer}>
+        {conversations.length == 0 && (
+          <View style={styles.loadingContainer}>
+            <Text style={styles.welcomeText}>
+              Hi, press the button below to text someone from your contacts
+            </Text>
+            <TouchableOpacity style={styles.button} onPress={openNewChat}>
+              <Text style={styles.buttonText}>Contacts</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
     </Pressable>
   );
 };
--- src/screens/Chats/stylesChats.ts
+++ src/screens/Chats/stylesChats.ts
@@ -12,9 +12,11 @@ export const dark = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     position: "relative",
+    marginBottom: 10,
   },
   headerText: {
     fontSize: 24,
+    color: "white",
   },
   popupContainer: {
     position: "absolute",
@@ -24,9 +26,51 @@ export const dark = StyleSheet.create({
     padding: 15,
     borderRadius: 5,
     gap: 10,
+    zIndex: 5,
   },
   popupText: {
+    color: "white",
     fontSize: 16,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#333232",
+    borderRadius: 30,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    width: "90%",
+    alignSelf: "center",
+    zIndex: -1,
+  },
+  searchInput: {
+    flex: 1,
+    margin: 0,
+    fontSize: 15,
+    color: "white",
+  },
+  chatContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    width: "70%",
+    textAlign: "center",
+    fontSize: 16,
+    color: "white",
+  },
+  button: {
+    backgroundColor: "#60bf4b",
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
   },
 });
 
@@ -42,9 +86,11 @@ export const light = StyleSheet.create({
     alignItems: "center",
     justifyContent: "space-between",
     position: "relative",
+    marginBottom: 10,
   },
   headerText: {
     fontSize: 24,
+    color: "black",
   },
   popupContainer: {
     position: "absolute",
@@ -54,8 +100,52 @@ export const light = StyleSheet.create({
     padding: 15,
     borderRadius: 5,
     gap: 10,
+    zIndex: 5,
   },
   popupText: {
+    color: "black",
     fontSize: 16,
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#333232",
+    borderRadius: 30,
+    paddingHorizontal: 10,
+    marginVertical: 10,
+    width: "90%",
+    alignSelf: "center",
+    zIndex: -1,
+  },
+  searchInput: {
+    flex: 1,
+    margin: 0,
+    fontSize: 15,
+    color: "black",
+  },
+  chatContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  welcomeText: {
+    width: "70%",
+    textAlign: "center",
+    fontSize: 16,
+    color: "black",
+  },
+  button: {
+    backgroundColor: "#60bf4b",
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
   },
 });
