1ded2f69f4e29d860566f4a87aa08fc4f786f0e9
vishal <vishal.gxm@gmail.com>
2023-04-07 22:40:08 +0530

    showing selected contact information

--- src/screens/NewChat/NewChat.tsx
+++ src/screens/NewChat/NewChat.tsx
@@ -1,27 +1,56 @@
-import {View, Text} from "react-native";
-import React, {useEffect} from "react";
+import {
+  View,
+  Text,
+  Alert,
+  Pressable,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, {useState} from "react";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import {dark, light} from "./stylesNewChat";
 import {Contacts} from "../../components";
+import filterPhoneNumber from "../../components/helperFunctions/filterPhoneNumber";
+import {useSelector} from "react-redux";
+import fetchUserByPhoneNumber from "../../firebaseHelpers/fetchUserByPhoneNumber";
 
 const NewChat = ({navigation}: any) => {
-  let theme = "dark";
+  const [searchUser, setSearchUser] = useState<any>(null);
+  const [showPopup, setShowPopup] = useState(false);
+  const {theme} = useSelector((store: any) => store.theme);
 
   const styles = theme == "dark" ? dark : light;
 
-  const handleContactPress = (phoneNumber: string) => {
-    phoneNumber.replace(" ", "");
-    console.log(phoneNumber);
+  const handleContactPress = async (phoneNumber: string) => {
+    phoneNumber = filterPhoneNumber(phoneNumber);
+    try {
+      let response = await fetchUserByPhoneNumber(phoneNumber);
+      setSearchUser(response);
+      setShowPopup(true);
+    } catch (error: any) {
+      console.log("Error in NewChat.tsx/handleContactPress");
+      console.log(error);
+      Alert.alert("Error", error.message);
+    }
   };
 
   const navigateBack = () => {
     navigation.goBack();
   };
 
+  const closePopup = () => {
+    if (showPopup) {
+      setShowPopup(false);
+      setSearchUser(null);
+    }
+  };
+
+  const handleChat = () => {};
+
   return (
-    <View style={styles.main}>
+    <Pressable style={styles.main} onPress={closePopup}>
       <View style={styles.headerContainer}>
         <Ionicons
           name="arrow-back"
@@ -31,8 +60,44 @@ const NewChat = ({navigation}: any) => {
         />
         <Text style={styles.headerText}>Contacts</Text>
       </View>
-      <Contacts onPress={handleContactPress} />
-    </View>
+      <View style={styles.contactsContainer}>
+        <Contacts onPress={handleContactPress} />
+        {showPopup && (
+          <View style={styles.popupContainer}>
+            <View style={styles.popupContent}>
+              {searchUser ? (
+                <View style={styles.selectedUserContainer}>
+                  <Image
+                    source={{uri: searchUser?.photoURL}}
+                    style={styles.selectedUserImage}
+                  />
+                  <View style={styles.selectedUserInfo}>
+                    <Text style={styles.selectedUserName}>
+                      {searchUser.displayName}
+                    </Text>
+                    <TouchableOpacity
+                      style={styles.popupButton}
+                      onPress={handleChat}>
+                      <Text style={styles.popupButtonText}>Chat</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              ) : (
+                <>
+                  <Text style={styles.popupText}>
+                    The contact you've selected does not appear to be a user,
+                    please invite them
+                  </Text>
+                  <TouchableOpacity style={styles.popupButton}>
+                    <Text style={styles.popupButtonText}>Invite</Text>
+                  </TouchableOpacity>
+                </>
+              )}
+            </View>
+          </View>
+        )}
+      </View>
+    </Pressable>
   );
 };
 
--- src/screens/NewChat/stylesNewChat.ts
+++ src/screens/NewChat/stylesNewChat.ts
@@ -15,6 +15,56 @@ export const dark = StyleSheet.create({
     fontSize: 25,
     color: "white",
   },
+  contactsContainer: {
+    flex: 1,
+    position: "relative",
+  },
+  popupContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: "10%",
+  },
+  popupContent: {
+    borderRadius: 5,
+    backgroundColor: "#333232",
+    padding: 15,
+  },
+  popupText: {
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 5,
+  },
+  popupButton: {
+    backgroundColor: "#60bf4b",
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: "center",
+  },
+  popupButtonText: {
+    fontSize: 15,
+    color: "white",
+  },
+  selectedUserContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+  selectedUserImage: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+  },
+  selectedUserInfo: {},
+  selectedUserName: {
+    fontSize: 17,
+    fontWeight: "bold",
+  },
 });
 
 export const light = StyleSheet.create({
@@ -31,5 +81,56 @@ export const light = StyleSheet.create({
   headerText: {
     fontSize: 25,
     color: "black",
+  },
+  contactsContainer: {
+    flex: 1,
+    position: "relative",
+  },
+  popupContainer: {
+    position: "absolute",
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingHorizontal: "10%",
+  },
+  popupContent: {
+    borderRadius: 5,
+    backgroundColor: "#DDF7E3",
+    padding: 15,
+  },
+  popupText: {
+    fontSize: 15,
+    textAlign: "center",
+    marginBottom: 5,
+    color: "black",
+  },
+  popupButton: {
+    backgroundColor: "#40513B",
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    alignSelf: "center",
+  },
+  popupButtonText: {
+    fontSize: 15,
+    color: "white",
+  },
+  selectedUserContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 20,
+  },
+  selectedUserImage: {
+    height: 100,
+    width: 100,
+    borderRadius: 50,
+  },
+  selectedUserInfo: {},
+  selectedUserName: {
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
